717ab1e874cb60706cf30baead9ad67336a676ea
Mickael GARDET <m.gardet@overkiz.com>
2017-12-01 16:58:59 +0100

    last STM32F3DISCOVERY card have new jatg usb vid/pid.

--- scripts/flash.sh
+++ scripts/flash.sh
@@ -5,11 +5,14 @@ prog="${1:-../build-stm32/stm32.elf}"
 scripts="-s /usr/share/openocd/scripts"
 interface="-f interface/stlink-v2.cfg"
 board="-f board/stm32f3discovery.cfg"
+new_board='-c "hla_vid_pid 0x0483 0x374b"'
 
 echo "prog $prog"
 
+openocd -f interface/stlink-v2.cfg -f board/stm32f3discovery.cfg -c "hla_vid_pid 0x0483 0x374b" -c "program $prog"
 
-openocd $scripts $interface $board -c init -c"reset init" -c "flash write_image erase $prog" -c reset -c shutdown
+
+#openocd $scripts $interface $board $new_board -c init -c "reset init" -c "flash write_image erase $prog" -c reset -c shutdown
 
 #openocd $scripts $interface $board  -c "program $prog  verify reset exit" -c "shutdown"
 
